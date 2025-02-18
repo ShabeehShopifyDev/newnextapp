d2b2bfbb6502d9a79fa0e64a940a8a73f34b3ba6
Shabeeh <shabeeh.techintegrations@gmail.com>
2025-02-18 21:58:23 +0500

    Made side bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
 import Brandlogo from './Brandlogo'
 import Navbar from './Navbar'
 import Otheractions from './Otheractions'
 import { Menu } from 'lucide-react'
 
 export default function Header() {
+  useEffect(() => {
+    document.querySelector("header .bars").addEventListener("click", () => {
+      document.querySelector(".header_wrapper .navbar").classList.toggle("active");
+    });
+    
+    document.body.addEventListener("click", (e) => {
+      if (!e.target.closest(".header_wrapper .navbar") && !e.target.closest("header .bars")) {
+        document.querySelector(".header_wrapper .navbar").classList.remove("active");
+      }
+    });
+  }, []);
+
   return (
     <header>
       <div className="header_wrapper container has__flex items__center">
